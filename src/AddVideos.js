863d3b8b0c8924b322cf920cec38433a7133a310
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-10-15 09:21:27 +0530

    bits of changes

--- src/AddVideos.js
+++ src/AddVideos.js
@@ -1,6 +1,8 @@
 import React,{useState} from 'react';
 import db from "./firebase";
 import "./AddVideos.css";
+import firebase from "firebase";
+import {Input} from "@material-ui/core";
 
 function AddVideos() {
     const [title,setTitle]=useState('');
@@ -9,6 +11,11 @@ function AddVideos() {
     const [channelImage,setChannelImage]=useState('');
     const [channel,setChannel]=useState('');
     const [video,setVideo]=useState('');
+    const handleFileChosen = (e) => {
+        if (e.target.files[0]){
+            setVideo(e.target.files[0])
+        }
+}
     const handleSubmit = (e) => {
         e.preventDefault();
         //db stuff
@@ -36,15 +43,17 @@ function AddVideos() {
        setVideo('');
     }
 
+
     return (
         <div className="addVideos">
             <form className="addVideos__form">
             <input type="text" value={title} onChange={(e)=> setTitle(e.target.value)}  className="addVideos__input" placeholder="Title of your video" />
             <input type="text" value={views} onChange={(e)=> setViews(e.target.value)}  className="addVideos__input" placeholder="no. of views" />
-            <input type="text" value={timestamp} onChange={(e)=> setTimestamp(e.target.value)}  className="addVideos__input" placeholder="timestamp" />
+            <input type="text" value={timestamp} onChange={(e)=> setTimestamp(e.target.value)}  className="addVideos__input" placeholder="Enter the date and time you are posting " /> 
             <input type="text" value={channelImage} onChange={(e)=> setChannelImage(e.target.value)}  className="addVideos__input" placeholder="channel Image URL" />
             <input type="text" value={channel} onChange={(e)=> setChannel(e.target.value)}  className="addVideos__input" placeholder="channel Name" />
-            <input type="text" value={video} onChange={(e)=> setVideo(e.target.value)}  className="addVideos__input" placeholder="Video URL" />
+           <input type="text" value={video} onChange={(e)=> setVideo(e.target.value)}  className="addVideos__input" placeholder="Video URL" /> 
+            {/*<Input type="file" value={video} onChange={handleFileChosen} className="addvideos__input" placeholder="video " />*/}
                   <button className="addVideos__btn" onClick={handleSubmit} type="submit">POST VIDEO</button>
             </form>
         </div>
